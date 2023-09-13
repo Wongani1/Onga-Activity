9a2ebed60795c746e86129726514d18b8a7ea625
Wongani Kaunda <wonganikaunda2@gmail.com>
2023-09-14 01:14:46 +0200

    Add files via upload

---
+++ pages/analyze/analyze_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AnalyzePage extends StatefulWidget {
+  static const String routeName = "/analyze";
+  @override
+  _AnalyzePageState createState() => _AnalyzePageState();
+}
+
+class _AnalyzePageState extends State<AnalyzePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
