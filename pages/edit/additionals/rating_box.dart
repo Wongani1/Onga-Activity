6dd510d5cbdedde606faa22f75573a6f56518e7c
Wongani Kaunda <wonganikaunda2@gmail.com>
2023-09-14 01:18:30 +0200

    Add files via upload

---
+++ pages/edit/additionals/rating_box.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+class Num {
+  int num = 10;
+}
+
+class RatingBox extends StatefulWidget {
+  @override
+  const RatingBox({
+    this.zrating,
+  });
+
+  final String zrating;
+
+  _RatingBoxState createState() => _RatingBoxState();
+}
+
+class _RatingBoxState extends State<RatingBox> {
+  int _rating = 0;
+  TextEditingController teRatingController = TextEditingController();
+
+  void _setRatingAsOne() {
+    setState(() {
+      _rating = 1;
+    });
+  }
+
+  void _setRatingAsTwo() {
+    setState(() {
+      _rating = 2;
+    });
+  }
+
+  void _setRatingAsThree() {
+    setState(() {
+      _rating = 3;
+    });
+  }
+
+  void _setRatingAsFour() {
+    setState(() {
+      _rating = 4;
+    });
+  }
+
+  void _setRatingAsFive() {
+    setState(() {
+      _rating = 5;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double _size = 20;
+    print(_rating);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 1
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.black,
+            onPressed: _setRatingAsOne,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 2
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.black,
+            onPressed: _setRatingAsTwo,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 3
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.black,
+            onPressed: _setRatingAsThree,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 4
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.black,
+            onPressed: _setRatingAsFour,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 5
+                ? Icon(
+                    Icons.star,
+                    size: _size,
+                  )
+                : Icon(
+                    Icons.star_border,
+                    size: _size,
+                  )),
+            color: Colors.black,
+            onPressed: _setRatingAsFive,
+            iconSize: _size,
+          ),
+        ),
+        Padding(padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 42.0)),
+      ],
+    );
+  }
+}
